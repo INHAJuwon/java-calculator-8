2c270bb3db81b5c117bee23aabd0900fb1042e37
ChoiJuwon <aowlrdofhdn@naver.com>
2025-10-18 18:32:58 +0900

    feat(Calculator): Calculator 클래스 구현 덧셈 기능을 수행할 Calculator 클래스와 메서드들을 구현
    - Calculator.java: 문자열을 입력받아 계산을 수행하는 calculate() 메서드 정의
    - 분리된 문문자열 배열의 합계를 구하는 sum() 메서드 구현
    - 숫자인지 확인(checkStr), 빈 문자열이나 null을 처리(blankOrEmpty), 음수 확인(checkNegative), 유효성 검사 로직을 private으로 구현
    
    현재는 입력 문자열을 구분하는 클래스들이 정의되지 않았다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,11 +7,11 @@ public class Application {
         // TODO: 프로그램 구현
         Input input = new Input();
         Output result = new Output();
-        //계산을 하는 class Calculator 구현
+        Calculator calculator = new Calculator();
 
         String str = input.getInput();
-        //Calculator에서 계산된 결과값을 int output에 저장
-        //result.printResult(output);
+        int output = calculator.calculate(str);
+        result.printResult(output);
         Console.close();
     }
 }
---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,42 @@
+package calculator;
+
+public class Calculator {
+    public int calculate(String str){
+        // 구분된 문자열과 구분자를 저장해두는 Parameter 설정
+        // numbers= Parameter의 구분된 문자열
+
+        //return sum(numbers);
+    }
+    private int sum(String[] numbs) {
+        if(numbs == null){
+            return 0;
+        }
+
+        int sum = 0;
+
+        for (String s : numbs) {
+            int add = checkStr(s);
+            checkNegative(add);
+            sum += add;
+        }
+
+        return sum;
+    }
+    private int checkStr(String s){
+        try{
+            return blankOrEmpty(s);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("입력이 숫자가 아님");
+        }
+    }
+    private int blankOrEmpty(String str){
+        if(str.isBlank())
+            return 0;
+        return Integer.parseInt(str);
+    }
+    private void checkNegative(int i){
+        if(i < 0){
+            throw new IllegalArgumentException("음수");
+        }
+    }
+}
